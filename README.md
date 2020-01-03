6564ca2e1145200d86db6d02a9b5270416f038d7
Stanisław Nieradko <47383643+KanarekLife@users.noreply.github.com>
2020-01-03 20:44:03 +0100

    Updated setup-script to better work on ubuntu/debian/fedora based distros

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Mir3k
-
-### Project Setup (DEV)
+## Project Setup (DEV)
 To setup project simply run `setup-dev.sh` script (before set propper execution priviliges using chmod command). Script will also create `run.sh` which will allow you to run program easily.
-=======
-
-### Credits
+## Credits
 Lexu
--- setup-dev.sh
+++ setup-dev.sh
@@ -12,11 +12,38 @@ _  _ ____ _  _ ____ ____ ____ _  _ _    _ ____ ____
 |_/  |__| |\ | |__| |__/ |___ |_/  |    | |___ |___ 
 | \_ |  | | \| |  | |  \ |___ | \_ |___ | |    |___ 
                                                     "
+if [ "$EUID" -ne 0 ]
+  then echo "[!] Please run as root"
+  exit
+fi
+PIP=pip
+PYTHON=python
+OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+FOUND="false"
+if [ "$OS" == "\"Ubuntu\"" ] || [ "$OS" == "\"Debian GNU/Linux\"" ] ; then
+    echo "[i] Detected Debian or Ubuntu based system"
+    apt-get update
+    apt-get install python3-venv python3-pip python3 -y
+    PIP=pip3
+    PYTHON=python3
+    FOUND="true"
+fi
+if [ "$OS" == "Fedora" ] ; then
+    echo "[i] Detected Fedora based system"
+    dnf update
+    dnf install python pip
+    FOUND="true"
+fi
+if [ "$FOUND" == "false" ] ; then
+    echo "[!] Using unsupported distro! Install python3 and python3-pip and set aliases for them (python and pip) and then restart this script!"
+fi
 echo "[+] Installing virtualenv"
-pip install virtualenv --user
+$PIP install virtualenv --user
 echo "[+] Creating venv folder"
-python -m venv venv
+$PYTHON -m venv venv
 echo "[+] Installing dependencies"
+./venv/bin/pip install wheel
 ./venv/bin/pip install chatterbot fbchat-asyncio discord praw requests googletrans chatterbot.corpus
+echo "[+] Creating run.sh file"
 printf "#!/bin/bash\n./venv/bin/python disc.py" > run.sh
 echo "[+] Done"
